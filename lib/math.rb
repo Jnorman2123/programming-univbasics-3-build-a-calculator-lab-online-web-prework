c2b24cf9f1629fe944a7e7d3379f3d0d04b7fb4a
Jonathan Norman <jnorman2123@gmail.com>
2019-09-24 20:45:21 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,30 +1,14 @@
 def addition(num1, num2)
-  num1 + num2
-end
+  sum = num1 + num2
+  sum 
+end 
 
-def subtraction(num1, num2)
-  num1 - num2
-end
 
-def division(num1, num2)
-  num1 / num2
-end
 
-def multiplication(num1, num2)
-  num1 * num2
-end
 
-def modulo(num1, num2)
-  num1 % num2
-end
 
-def square_root(num)
-  Math.sqrt(num)
-end
 
-addition(5,4)
-subtraction(10,5)
-division(50,2)
-multiplication(4,30)
-modulo(34,5)
-square_root(81)
+
+
+
+
